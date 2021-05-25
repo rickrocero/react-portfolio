2fc7c5a4fbf94b35c322bf6837369ae4bf7c2fcd
ricroc <rickrocero@gmail.com>
2021-05-25 16:02:01 -0700

    added github and linkedin links to footer

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ export default function About() {
     return (
         <div>
             <div className="text-center">
-                <img src="../images/me-donut.jpg" className="rounded about-img" alt="Rick eating a donut" />
+                <img src="" className="rounded about-img" alt="Rick eating a donut" />
             </div>
             <h1 className="text-center">Hello, I am Rick Rocero.</h1>
             <p className="text-center">
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -29,20 +29,5 @@ export default function NavBar() {
                 </Link>
             </li>
         </ul>
-
-           //     <ul className="nav justify-content-center">
-    //         <li className="nav-item">
-    //             <a className="nav-link disabled" aria-current="page" href="#">Phone | 360.724.8730</a>
-    //         </li>
-    //         <li className="nav-item">
-    //             <a className="nav-link disabled" href="#">Email | rickrocero@gmail.com</a>
-    //         </li>
-    //         <li className="nav-item">
-    //             <a className="nav-link" href="https://github.com/rickrocero" target="_blank">GitHub</a>
-    //         </li>
-    //         <li className="nav-item">
-    //             <a className="nav-link" href="https://www.linkedin.com/in/rick-rocero-b8a795202/" target="_blank">LinkedIn</a>
-    //         </li>
-    //   </ul>
     );
 }
